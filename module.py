3e965c2128c4af4e8446a60bd864c1732d4fa6af
Diante Dowdell <diante.dowdell99@gmail.com>
2022-09-28 23:24:29 -0500

    completed homework, first commit

---
+++ module.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def square_footage(length, width):
+    '''Returns the square footage of a house given the length and width as parameters'''
+    return length * width
+
+def circum_of_circle(radius):
+    '''Returns the circumference of a circle with the given radius as a parameter'''
+    return 2 * pi * radius
